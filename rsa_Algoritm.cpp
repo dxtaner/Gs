6af165f416ab1a24bc26355a4c597ae2304e8488
taner <tanerozer16@gmail.com>
2023-09-08 12:40:47 +0300

    edited

--- rsa_Algoritm.cpp
+++ rsa_Algoritm.cpp
@@ -1,60 +1,54 @@
 #include<stdio.h>
 #include<math.h> 
-#include<string.h>
-#include<stdlib.h>
 
 int bul(int a,int b);
-int asalmi(int sayi);
-
 
 int main(){
-	int p,q,n,totient,e;
 	
-	printf("Birinci asal sayi :");
-	scanf("%d",&p);
+	// Two random prime numbers 
+    double p = 3; 
+    double q = 7; 
+  
+    // First part of public key: 
+    double n = p*q; 
+  
+    // Finding other part of public key. 
+    // e stands for encrypt 
+    double e = 2; 
+    double fonk = (p-1)*(q-1); 
+    while (e < fonk) 
+    { 
+        // e must be co-prime to phi and 
+        // smaller than phi. 
+        if (bul(e, fonk)==1) 
+            break; 
+        else
+            e++; 
+    } 
+  
+    // Private key (d stands for decrypt) 
+    // choosing d such that it satisfies 
+    // d*e = 1 + k * totient 
+    int k = 2;  // A constant value 
+    double d = (1 + (k*fonk))/e; 
+  
+    // Message to be encrypted 
+    double msg = 20; 
+  
+    printf("Message data = %lf", msg); 
+  
+    // Encryption c = (msg ^ e) % n 
+    double c = pow(msg, e); 
+    c = fmod(c, n); 
+    printf("\nEncrypted data = %lf", c); 
+  
+    // Decryption m = (c ^ d) % n 
+    double m = pow(c, d); 
+    m = fmod(m, n); 
+    printf("\nOriginal Message Sent = %lf", m); 
+  
+    return 0; 
 	
-	if(asalmi(p)==0){
-		printf("asal sayi girmediniz!");
-		exit(1);
-	}
-	
-	printf("ikinci asal sayi :");
-	scanf("%d",&q);
-	
-	if(asalmi(q)==0 || q==p){
-		printf("sayiniz asl deðil ve ya p degeri ile ayni ");
-		exit(1);	
-	}
-	
-	n=p*q;
-	totient=(p-1)*(q-1);
-	e=2;
-		while(e<totient){
-			if(bul(e,totient)==1){
-			break;
-			}	
-			else{
-				e++;
-			}
-		} 
- 	
-	int k = 2; 
-    double d = (1 + (k*totient)/e);
- 
-    double msg = 20;
- 
-    printf("Message data = %lf", msg);
- 
-    //Þifreleme
-    double c = pow(msg, e);
-    c = fmod(c, n);
-    printf("\nEncrypted data = %lf", c);
- 
-    // Deþifreleme  
-    m = bul(m, n);
-    printf("\nOriginal Message Sent = %lf", m);		
-	
-return 0;	
 }
 
 int bul(int a,int b){
@@ -65,16 +59,6 @@ int bul(int a,int b){
 	int c=a/b;
 	a=b;
 	b=kalan;
-	return bul(a,b);	
-}
-
-int asalmi(int sayi){
-	int i,temp=sayi;
-	for(i=2 ; i<=sayi/2 ; i++){
-		if(sayi%i==0)
-		{
-			return 0;
-		}
-	}
-	return 1;
+	return bul(a,b);
+	
 }
