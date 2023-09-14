b42047e3e9511c059c18c833b79a83a7af575094
taner <tanerozer16@gmail.com>
2023-09-14 14:32:00 +0300

    Create Program.cs

---
+++ HarfAndKelime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir cümle yazınız: ");
+        string cumle = Console.ReadLine();
+
+        int harfSayisi = 0;
+        int kelimeSayisi = 0;
+
+        // Boşluklara göre cümleyi kelimelere ayır
+        string[] kelimeler = cumle.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string kelime in kelimeler)
+        {
+            kelimeSayisi++; // Her kelimeyi say
+
+            foreach (char karakter in kelime)
+            {
+                if (char.IsLetter(karakter))
+                {
+                    harfSayisi++; // Her harfi say
+                }
+            }
+        }
+
+        Console.WriteLine($"Toplam Harf Sayısı: {harfSayisi}");
+        Console.WriteLine($"Toplam Kelime Sayısı: {kelimeSayisi}");
+    }
+}
