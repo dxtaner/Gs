47f83e1151d24bcfa3da4d279d281577fe522522
taner <tanerozer16@gmail.com>
2023-09-15 15:04:32 +0300

    Create Program.cs

---
+++ HarfNAdet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Kaç kelime gireceksiniz? (pozitif bir tam sayı girin): ");
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            string[] kelimeler = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Kelime {i + 1}: ");
+                kelimeler[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nGirilen Kelimeler (Sondan Başa Doğru):");
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.WriteLine(kelimeler[i]);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz giriş! Pozitif bir tam sayı girin.");
+        }
+    }
+}
